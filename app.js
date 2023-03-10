99d60c076ac2fce592dda0de5c06c41fd7e8600d
guruece24 <guruece24@gmail.com>
2023-03-10 15:15:35 +0530

    schema

--- app.js
+++ app.js
@@ -10,19 +10,50 @@ const api = process.env.API_URL
 app.use(express.json())
 app.use(morgan('tiny'))
 
-app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some url',
+const productSchema = new mongoose.Schema({
+    name: String, // String is shorthand for {type: String}
+    image: String,
+    countInStock: {
+        type: Number,
+        required: true,
+    },
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+app.get(`${api}/products`, async (req, res) => {
+    const productList = await Product.find()
+
+    if (!productList) {
+        res.status(500).json({
+            error: err,
+            success: false,
+        })
     }
-    res.send(product)
+    res.send(productList)
+    // res.status(200).json(productList)
 })
 
 app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body
-    console.log(newProduct)
-    res.send(newProduct)
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock,
+    })
+
+    product
+        .save()
+        .then((createdProduct) => {
+            res.status(201).json(createdProduct)
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: err,
+                success: false,
+            })
+        })
+
+    console.log(product)
 })
 
 mongoose
