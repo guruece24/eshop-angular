0747174db58897704131f2e43d6ebb3e6bb13ba8
guruece24 <guruece24@gmail.com>
2023-03-15 14:25:19 +0530

    categoriespost

--- models/category.js
+++ models/category.js
@@ -1,7 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const categorySchema = mongoose.Schema({
-
+    name: {
+        type: String,
+        required: true,
+    },
+    icon: {
+        type: String,
+    },
+    color: {
+        type: String,
+    },
+    // icon: {
+    //     type: String,
+    //     required: true,
+    // },
+    // Image: {
+    //     type: String,
+    //     required: true,
+    // },
 })
 
-exports.Category = mongoose.model('Category', categorySchema);
+exports.Category = mongoose.model('Category', categorySchema)
--- routes/categories.js
+++ routes/categories.js
@@ -1,14 +1,28 @@
-const {Category} = require('../models/category');
-const express = require('express');
-const router = express.Router();
+const { Category } = require('../models/category')
+const express = require('express')
+const router = express.Router()
 
-router.get(`/`, async (req, res) =>{
-    const categoryList = await Category.find();
+router.get(`/`, async (req, res) => {
+    const categoryList = await Category.find()
 
-    if(!categoryList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(categoryList);
+    if (!categoryList) {
+        res.status(500).json({ success: false })
+    }
+    res.send(categoryList)
 })
 
-module.exports =router;
+router.post(`/`, async (req, res) => {
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color,
+    })
+    category = await category.save()
+
+    if (!category)
+        return res.status(404).send('the category cannot be created!')
+
+    res.send(category)
+})
+
+module.exports = router
