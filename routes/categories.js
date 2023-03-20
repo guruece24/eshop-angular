22f0e5d9c50a8151e12d37697e217a5e7bd929b5
guruece24 <guruece24@gmail.com>
2023-03-20 18:47:09 +0530

    put

--- routes/categories.js
+++ routes/categories.js
@@ -8,16 +8,19 @@ router.get(`/`, async (req, res) => {
     if (!categoryList) {
         res.status(500).json({ success: false })
     }
-    res.status(200).send(categoryList);
+    res.status(200).send(categoryList)
 })
 
 router.get(`/:id`, async (req, res) => {
-    const category = await Category.findById(req.params.id);
+    const category = await Category.findById(req.params.id)
 
     if (!category) {
-        res.status(500).json({ success: false, message: 'the category with the given Id is not found' })
+        res.status(500).json({
+            success: false,
+            message: 'the category with the given Id is not found',
+        })
     }
-    res.status(200).send(category);
+    res.status(200).send(category)
 })
 
 router.post(`/`, async (req, res) => {
@@ -27,6 +30,23 @@ router.post(`/`, async (req, res) => {
         color: req.body.color,
     })
     category = await category.save()
+
+    if (!category)
+        return res.status(404).send('the category cannot be created!')
+
+    res.send(category)
+})
+
+router.put('/:id', async (req, res) => {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color,
+        },
+        { new: true }
+    )
 
     if (!category)
         return res.status(404).send('the category cannot be created!')
