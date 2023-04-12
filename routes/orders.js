329e451e16076a51ae9d2705fb72435c73325dec
guruece24 <guruece24@gmail.com>
2023-04-12 15:46:58 +0530

    orderpost

--- routes/orders.js
+++ routes/orders.js
@@ -1,14 +1,54 @@
-const {Order} = require('../models/order');
-const express = require('express');
-const router = express.Router();
+const { Order } = require('../models/order')
+const express = require('express')
+const { OrderItem } = require('../models/order-item')
+const router = express.Router()
 
-router.get(`/`, async (req, res) =>{
-    const orderList = await Order.find();
+router.get(`/`, async (req, res) => {
+    const orderList = await Order.find()
+        .populate('user', 'name')
+        .sort({ dateOrdered: -1 })
 
-    if(!orderList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(orderList);
+    if (!orderList) {
+        res.status(500).json({ success: false })
+    }
+    res.send(orderList)
 })
 
-module.exports =router;
+router.post('/', async (req, res) => {
+    const orderItemsIds = Promise.all(
+        req.body.orderItems.map(async (orderitem) => {
+            let newOrderItem = new OrderItem({
+                quantity: orderitem.quantity,
+                product: orderitem.product,
+            })
+
+            newOrderItem = await newOrderItem.save()
+
+            return newOrderItem._id
+        })
+    )
+
+    const orderItemsIdsResolved = await orderItemsIds
+
+    console.log(orderItemsIdsResolved)
+
+    let order = new Order({
+        orderItems: orderItemsIdsResolved,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: req.body.totalPrice,
+        user: req.body.user,
+    })
+    order = await order.save()
+
+    if (!order) return res.status(400).send('the order cannot be created!')
+
+    res.status(200).send(order)
+})
+
+module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -102,15 +102,33 @@ router.post(`/login`, async (req, res) => {
     return res.status(200).send(user)
 })
 
-router.get(`/get/count`, async (req, res) =>{
+router.delete('/:id', (req, res) => {
+    User.findByIdAndRemove(req.params.id)
+        .then((user) => {
+            if (user) {
+                return res
+                    .status(200)
+                    .json({ success: true, message: 'the user is deleted!' })
+            } else {
+                return res
+                    .status(404)
+                    .json({ success: false, message: 'user not found!' })
+            }
+        })
+        .catch((err) => {
+            return res.status(500).json({ success: false, error: err })
+        })
+})
+
+router.get(`/get/count`, async (req, res) => {
     const userCount = await User.countDocuments()
 
-    if(!userCount) {
-        res.status(500).json({success: false})
-    } 
+    if (!userCount) {
+        res.status(500).json({ success: false })
+    }
     res.send({
-        userCount: userCount
-    });
+        userCount: userCount,
+    })
 })
 
 module.exports = router
