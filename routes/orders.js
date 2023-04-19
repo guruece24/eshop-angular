3887b334773a16a756c19b10a3f98a1270543556
guruece24 <guruece24@gmail.com>
2023-04-19 14:52:44 +0530

    ordersdelete

--- routes/orders.js
+++ routes/orders.js
@@ -14,6 +14,23 @@ router.get(`/`, async (req, res) => {
     res.send(orderList)
 })
 
+router.get(`/:id`, async (req, res) => {
+    const order = await Order.findById(req.params.id)
+        .populate('user', 'name')
+        .populate({
+            path: 'orderItems',
+            populate: {
+                path: 'product',
+                populate: 'category',
+            },
+        })
+
+    if (!order) {
+        res.status(500).json({ success: false })
+    }
+    res.send(order)
+})
+
 router.post('/', async (req, res) => {
     const orderItemsIds = Promise.all(
         req.body.orderItems.map(async (orderitem) => {
@@ -50,5 +67,40 @@ router.post('/', async (req, res) => {
 
     res.status(200).send(order)
 })
+
+router.put('/:id', async (req, res) => {
+    const order = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+            status: req.body.status,
+        },
+        { new: true }
+    )
+
+    if (!order) return res.status(400).send('the order cannot be updated!')
+
+    res.send(order)
+})
+
+router.delete("/:id", (req, res) => {
+    Order.findByIdAndRemove(req.params.id)
+      .then(async (order) => {
+        if (order) {
+          await order.orderItems.map(async (orderItem) => {
+            await OrderItem.findByIdAndRemove(orderItem);
+          });
+          return res
+            .status(200)
+            .json({ success: true, message: "the order is deleted!" });
+        } else {
+          return res
+            .status(404)
+            .json({ success: false, message: "order not found!" });
+        }
+      })
+      .catch((err) => {
+        return res.status(500).json({ success: false, error: err });
+      });
+  });
 
 module.exports = router
